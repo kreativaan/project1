e39fc5c8396f210c083c8e50b818a90d370a71b9
kreativaan <ivankovacevic123456@gmail.com>
2025-05-21 10:05:23 +0200

    first commit

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,20 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+            <x-tag>tag</x-tag>
+
+        </div>
+        <img src="http://placehold.co/42x42" alt="" class="rounded-xl">
+    </div>
+
+</div>
